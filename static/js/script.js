445eb4b7466a386b340aef236a1d68203eebdc81
Eklera <lera.googlit@gmail.com>
2020-04-19 19:18:15 +0300

    fix js domain 2

--- static/js/script.js
+++ static/js/script.js
@@ -175,7 +175,7 @@ function SendFile() {
     $.ajax({
         // url: "https://6f412c5e-e568-4592-bcde-2f9f011ad67a.mock.pstmn.io",
         // url: "https://demo2398178.mockable.io/",
-        url: "http://sisond.wndenis.space/",
+        url: window.location.href,
         type: "POST",
         data: fd,
         processData: false,  // tell jQuery not to process the data
