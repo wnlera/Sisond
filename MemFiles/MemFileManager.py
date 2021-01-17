ad1b2de215cd59d7801f949524cc731757f8ad96
Valeriya Timofeeva <lera.googlit@gmail.com>
2021-01-18 02:28:20 +0300

    Test

--- MemFiles/MemFileManager.py
+++ MemFiles/MemFileManager.py
@@ -17,6 +17,7 @@ class MemFileManager:
     def get_file(self, uuid):
         self.remove_timeout_files()
         if uuid not in self.__files:
+            print(f"{uuid} not found in {self.__files.keys()}")
             return None
         file = self.__files[uuid]
         return file
@@ -27,5 +28,7 @@ class MemFileManager:
             if file.expired:
                 file.wipe()
                 to_remove.add(uuid)
+        if len(to_remove):
+            print(f"Removed {len(to_remove)} files")
         for elem in to_remove:
             self.__files.pop(elem)
